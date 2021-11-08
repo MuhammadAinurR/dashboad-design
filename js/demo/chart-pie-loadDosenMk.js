2acf3c392d22d73001321d02a091b5423d44322f
MuhammadAinurR <ainurmoh@gmail.com>
2021-11-08 08:55:01 +0700

    a

---
+++ js/demo/chart-pie-loadDosenMk.js
@@ -0,0 +1,37 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+(Chart.defaults.global.defaultFontFamily = 'Nunito'), '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+var ctx = document.getElementById('chartLoadDosenMk');
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ['<5', '5-8', '9-12', '>12'],
+    datasets: [
+      {
+        data: [55, 26, 8, 11],
+        backgroundColor: ['#8e78d6', '#30a4fc', '#30e8aa', '#febf44'],
+        hoverBackgroundColor: ['#5f5090', '#1f659a', '#1d9069', '#a97f2b'],
+        hoverBorderColor: 'rgba(234, 236, 1)',
+      },
+    ],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: 'rgb(255,255,255)',
+      bodyFontColor: '#858796',
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false,
+    },
+    cutoutPercentage: 80,
+  },
+});
---
+++ js/demo/chart-pie-pendidikanTerakhir.js
@@ -0,0 +1,37 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+(Chart.defaults.global.defaultFontFamily = 'Nunito'), '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+var ctx = document.getElementById('chartPendidikanTerakhirDosen');
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ['S2', 'S3'],
+    datasets: [
+      {
+        data: [60, 80],
+        backgroundColor: ['#6573c3', '#73c365'],
+        hoverBackgroundColor: ['#3f51b5', '#508846'],
+        hoverBorderColor: 'rgba(234, 236, 1)',
+      },
+    ],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: 'rgb(255,255,255)',
+      bodyFontColor: '#858796',
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false,
+    },
+    cutoutPercentage: 80,
+  },
+});
